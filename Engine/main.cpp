917ade5dfb58442dbd48704ffcc9224227738941
Margaret Dorsey <margaretdorsey94@gmail.com>
2015-12-22 13:27:18 -0500

    linker errors all resolved, ready to dig in for winter break and get this thing on the road

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -10,10 +10,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Windows.h>
-#include "include/glew.h"
-#include<GL/GLU.h>
-#include<GL/GL.h>
-#include "include/glfw3.h"
+#include"GL\glew.h"
+#include<GL\GLU.h>
+#include<GL\GL.h>
+#include"glfw3.h"
 //---------------------
 //variable declarations
 //---------------------
@@ -22,6 +22,7 @@
 const int windowWidth = 800;
 const int windowHeight = 600;
 const char* windowString = "Test Window";
+GLFWwindow* window;
 
 //function prototypes
 void init();
@@ -30,19 +31,33 @@ void update();
 int main(int argc, char** argv)
 {
   //initialize glfw library and make a window
-  glfwInit();
-  GLFWwindow* window = glfwCreateWindow(windowWidth,windowHeight, windowString, nullptr, nullptr);
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW. \n");
+		return -1; //exit failure
+	}
+  window = glfwCreateWindow(windowWidth,windowHeight, windowString, NULL, NULL);
  
+  if (window == NULL)
+  {
+	  fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not compatible with GL 3.3+.");
+	  glfwTerminate();
+	  return -1;
+  }
   //makes openGL context current for created window
-  glfwMakeContextCurrent(window);
-  //number of screen updates to wait before swapping buffers
-  glfwSwapInterval(1);
- 
+  glfwMakeContextCurrent(window); 
+  glewExperimental = true;
+  if (glewInit() != GLEW_OK) //Initialize GLEW
+  {
+	  fprintf(stderr, "Failed to initialize GLEW \n");
+	  return -1; //exit failure
+  }
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE); //keyboard input
   //initialize everything else
   init();
 
   //main loop
-  while(!glfwWindowShouldClose(window))
+  while(!glfwWindowShouldClose(window) && glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS)
     update();
 
   glfwTerminate(); //cleanup after we're done
@@ -51,11 +66,14 @@ int main(int argc, char** argv)
 //run this once
 void init()
 {
-	
+	glClearColor(0.392f, 0.584f, 0.929f, 1.0f); //cornflower blue, for nostalgia's sake.
 }
 
 //run once a frame
 void update()
 {
-
+	glClear(GL_COLOR_BUFFER_BIT);
+	//swap buffers and catch keyboard input
+	glfwSwapBuffers(window);
+	glfwPollEvents();
 }
