03b720ffa641098bd6de196ddecd4a0e5381b677
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-04 16:17:26 -0500

    cranked out a bmp reading function in the main, more texturing to come later

--- Wyvern/main.cpp
+++ Wyvern/main.cpp
@@ -2,9 +2,8 @@
 //Rochester Institute of Technology
 //main.cpp is going to be our sandbox during development
 //
-//Currently setting up OPENGL for use and initializing everything
-//and testing compilation with g++ and openGL. Most of this stuff will
-//be moved out of the main file and put into a master init function once it's ironed out.
+// Most of this stuff will
+//be moved out of the main file and put into a master or class once it's ironed out.
 
 //include the libraries
 #define _CRTDBG_MAP_ALLOC
@@ -34,6 +33,7 @@ GLuint colorbuffer;
 //function prototypes
 void init();
 void update();
+GLuint loadBMP(const char* imgpath);
 //main
 int main(int argc, char** argv)
 {
@@ -90,4 +90,53 @@ void update()
 	//swap buffers and catch keyboard input
 	glfwSwapBuffers(windowMaster->GetWindow());
 	glfwPollEvents();
+}
+
+GLuint loadBMP(const char* imgpath)
+{
+	unsigned char header[54]; //bmp files begin with a 54 byte header
+	uint dataPos; //position where the actual date begins
+	uint width, height;
+	uint imageSize; //width*height*3
+	//rgb data
+	unsigned char* data;
+	//open the file
+	FILE* file = fopen(imgpath, "rb");
+	if (!file){ fprintf(stderr, "Image could not be opened \n"); return false;}
+	if (fread(header, 1, 54, file) != 54)
+	{
+		fprintf(stderr, "Not a correct BMP file \n");
+		return false;
+	}
+	if (header[0] != 'B' || header[1] != 'M')
+	{
+		fprintf(stderr, "Not a correct BMP file \n");
+		return false;
+	}
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+
+	//some bmp files are misformatted, guess missing info
+	if (imageSize == 0)
+	{
+		imageSize = width*height * 3;
+	}
+	if (dataPos = 0)
+	{
+		dataPos = 54; //start after the bmp header
+	}
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	//bind the newly created texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	//give the image to open gl
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 }
