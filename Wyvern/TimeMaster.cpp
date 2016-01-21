c48f3824754946b899b265d8a84982913c2c982f
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-01-21 00:07:46 -0500

    starting in on the timemaster, deciding whether to implement it by hand or cheat and use glfw functions

--- Wyvern/TimeMaster.cpp
+++ Wyvern/TimeMaster.cpp
@@ -1,1 +1,55 @@
 #include"TimeMaster.h"
+TimeMaster* TimeMaster::m_instance = nullptr;
+
+TimeMaster* TimeMaster::GetInstance(void)
+{
+	if (m_instance == nullptr)
+	{
+		m_instance = new TimeMaster();
+	}
+	return m_instance;
+}
+void TimeMaster::ReleaseInstance(void)
+{
+	if (m_instance != nullptr)
+	{
+		delete m_instance;
+		m_instance = nullptr;
+	}
+}
+uint TimeMaster::GetFPS(void)
+{
+
+}
+void TimeMaster::UpdateTime(void)
+{
+
+}
+void TimeMaster::StartClock(uint p_clockIndex)
+{
+
+}
+float TimeMaster::TimeDiff(uint p_clockIndex)
+{
+
+}
+bool TimeMaster::Countdown(uint p_clockIndex, float p_time)
+{
+
+}
+TimeMaster::TimeMaster(void)
+{
+
+}
+TimeMaster::TimeMaster(TimeMaster const& other)
+{
+
+}
+TimeMaster& TimeMaster::operator=(TimeMaster const& other)
+{
+	return *this;
+}
+TimeMaster::~TimeMaster(void)
+{
+
+}
--- Wyvern/TimeMaster.h
+++ Wyvern/TimeMaster.h
@@ -4,6 +4,10 @@
 
 class TimeMaster{
 
+	struct Clock
+	{
+
+	};
 	DWORD timerStart = 0;
 	DWORD startingTime = 0;
 	DWORD lastCalled = 0;
@@ -11,22 +15,22 @@ class TimeMaster{
 	uint fps = 60; //frames per second
 	int frames = 0; //keep track of frames
 
-	std::vector<DWORD> clock; //clock list
-
 public:
+	static TimeMaster* GetInstance(void);
+	static void ReleaseInstance(void);
+	uint CreateClock(void);
 	uint GetFPS(void);
 	void UpdateTime(void); //update the current time
-	void StartClock(uint clock); //starts a time count for the clock at given index
-	float TimeDiff(uint clock); //time difference since the last time this method was called for the given clock
-	bool Countdown(uint clock, float time, bool repeat); //counts down on the given clock, returns true when count is up
+	void StartClock(uint p_clockIndex); //starts a time count for the clock at given index
+	float TimeDiff(uint p_clockIndex); //time difference since the last time this method was called for the given clock
+	bool Countdown(uint p_clockIndex, float p_time); //counts down on the given clock, returns true when count is up
 
 private:
 	TimeMaster(void); //constructor
 	TimeMaster(TimeMaster const& other); //copy constructor
 	TimeMaster & operator=(TimeMaster const& other); //copy assignment operator
 	~TimeMaster(void); //destructor
-
-	void Release(void); //give up our memory
-	void Init(void); //initialize
+	static TimeMaster* m_instance;
+	std::vector<Clock> m_clocks; //clock list
 };
 #endif
