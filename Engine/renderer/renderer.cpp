5bb8a3ee83f6caf737af1a09be25e1ff0e2c5b6b
Margaret Dorsey <margaretdorsey94@gmail.com>
2017-07-31 20:49:34 -0400

    some work on starting the renderer

---
+++ Engine/renderer/renderer.cpp
@@ -0,0 +1,82 @@
+#include "renderer.h"
+
+
+Renderer::Renderer()
+{
+	m_d3d = 0;
+}
+
+
+Renderer::Renderer(const Renderer& other)
+{
+}
+
+
+Renderer::~Renderer()
+{
+}
+
+
+bool Renderer::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+
+	bool result;
+
+
+	// Create the Direct3D object.
+	m_d3d = new Dx11Manager;
+	if (!m_d3d)
+	{
+		return false;
+	}
+
+	// Initialize the Direct3D object.
+	result = m_d3d->Initialize(screenWidth, screenHeight, k_vsync_enabled, hwnd, k_fullscreen, k_screen_depth, k_screen_near);
+	if (!result)
+	{
+		MessageBox(hwnd, (LPCSTR)L"Could not initialize Direct3D", (LPCSTR)L"Error", MB_OK);
+		return false;
+	}
+
+	return true;
+}
+
+
+void Renderer::Shutdown()
+{
+	// Release the Direct3D object.
+	if (m_d3d)
+	{
+		m_d3d->Shutdown();
+		delete m_d3d;
+		m_d3d = 0;
+	}
+	return;
+}
+
+
+bool Renderer::Frame()
+{
+	bool result;
+
+
+	// Render the graphics scene.
+	result = Render();
+	if (!result)
+	{
+		return false;
+	}
+	return true;
+}
+
+
+bool Renderer::Render()
+{
+	// Clear the buffers to begin the scene.
+	m_d3d->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
+
+
+	// Present the rendered scene to the screen.
+	m_d3d->EndScene();
+	return true;
+}
--- Engine/renderer/renderer.h
+++ Engine/renderer/renderer.h
@@ -5,5 +5,32 @@
 #define _RENDERER_H
 
 #include "text_renderer.h"
+#include "dx11/dx11.h"
+#include <Windows.h>
+
+const bool k_fullscreen = false;
+const bool k_vsync_enabled = true;
+const float k_screen_depth = 1000.0f;
+const float k_screen_near = 0.1f;
+
+
+class Renderer
+{
+public:
+	Renderer();
+	Renderer(const Renderer&);
+	~Renderer();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+
+private:
+	Dx11Manager* m_d3d;
+
+};
 
 #endif
